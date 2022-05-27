15515db319c0f82c3ead8711506836997643537b
Nadya752 <57626488+Nadya752@users.noreply.github.com>
2022-05-27 17:20:09 +1000

    HTML and CSS too

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
+//http://localhost:1234
 const express = require('express');
 const app = express();
-const PORT = 8888;
+const PORT = 1234;
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/dist'));
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/public/index.html')
+    res.sendFile(__dirname + '/dist/index.html')
 })
-let server = app.listen(PORT, function(){
-    console.log("App server is running on port 8888");
-});
+// let server = app.listen(PORT, function(){
+//     console.log(`App server is running on port ${PORT}`);
+// });
    
-   
+
    
    
    
